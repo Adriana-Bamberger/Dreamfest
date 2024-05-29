f08a16ed585bc150df8030f6c8056dc53b8bb2f3
Adriana Bamberger <adriana@bamberger.co.nz>
2024-05-29 13:29:57 +1200

    finnaly got the locations to display with help from aimee yay

--- server/routes/locations.ts
+++ server/routes/locations.ts
@@ -1,28 +1,15 @@
 import express from 'express'
 
 import * as db from '../db/index.ts'
+import LocationsList from '../../client/components/LocationsList.tsx'
 
 const router = express.Router()
 
 // GET /api/v1/locations
 router.get('/', async (req, res, next) => {
   try {
-    // TODO: Replace this with all of the locations in the database
-    const locations = [
-      {
-        id: 1,
-        name: 'TangleStage',
-        description:
-          'Not the biggest stage, but perhaps the most hip. Not the biggest stage, but perhaps the most hip. Not the biggest stage, but perhaps the most hip.',
-      },
-      {
-        id: 2,
-        name: 'Yella Yurt',
-        description:
-          "It's a freakin' yurt! Get in here! It's a freakin' yurt! Get in here! It's a freakin' yurt! Get in here! It's a freakin' yurt! Get in here!",
-      },
-    ]
-
+    // Done: Replace this with all of the locations in the database
+    const locations = await db.getAllLocations()
     res.json({ locations })
   } catch (e) {
     next(e)
