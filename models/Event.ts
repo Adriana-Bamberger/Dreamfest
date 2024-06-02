60718f3ca48a6629373ee19f461fc658474bed0a
Adriana Bamberger <adriana@bamberger.co.nz>
2024-06-03 11:25:23 +1200

    fixed error, spelling and order

--- models/Event.ts
+++ models/Event.ts
@@ -18,25 +18,8 @@ export interface EventWithLocation {
 export interface Event extends EventData {
   id: number
 }
-// Above are the ones that where already here, below the ones I've made with syntax suggestion from gemini for safety.
-
-export function changeFromEventData(eventData: EventData): DbEventData {
-  const { day, time, name, description } = eventData
-  const location_id = Number(eventData.locationId)
-  return { location_id, day, time, name, description } as DbEventData
-}
-
-export function changeToEvent(event: DbEvent): Event {
-  const { id, day, time, name, description } = event
-  const locationId = Number(event.location_id)
-  return { id, locationId, day, time, name, description } as Event
-}
-export function changeFromEvent(event: Event): DBEvent {
-  const { id, day, time, name, description } = event
-  const location_id = Number(event.locationId)
-  return { id, location_id, day, time, name, description } as DbEvent
-}
-
+// Above are the ones that where already here, 
+// Heres my ones
 export interface DbEventData {
   location_id: number
   day: string
@@ -48,3 +31,22 @@ export interface DbEventData {
 export interface DbEvent extends DbEventData {
   id: number
 }
+
+ // the ones I needed Gemini's help with the syntax for, fixxed the error.
+export function changeFromEventData(eventData: EventData): DbEventData {
+  const { day, time, name, description } = eventData
+  const location_id = Number(eventData.locationId)
+  return { location_id, day, time, name, description } as DbEventData
+}
+
+export function changeToEvent(event: DbEvent): Event {
+  const { id, day, time, name, description } = event
+  const locationId = Number(event.location_id)
+  return { id, locationId, day, time, name, description } as Event
+}
+export function changeFromEvent(event: Event): DbEvent {
+  const { id, day, time, name, description } = event
+  const location_id = Number(event.locationId)
+  return { id, location_id, day, time, name, description } as DbEvent
+}
+
