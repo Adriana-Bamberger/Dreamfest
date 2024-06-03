88a71d4f781e286a4ec6dcde83754289056613d8
Adriana Bamberger <adriana@bamberger.co.nz>
2024-06-03 15:46:59 +1200

    fixing routes

--- server/routes/schedule.ts
+++ server/routes/schedule.ts
@@ -1,5 +1,4 @@
 import express from 'express'
-
 import { validateDay } from './helpers.ts'
 import * as db from '../db/index.ts'
 
@@ -10,27 +9,9 @@ router.get('/:day', async (req, res, next) => {
   try {
     const day = validateDay(req.params.day)
     // TODO: replace this data with the results of calling db.getEventsByDay
-    const events = [
-      {
-        id: 1,
-        day: 'friday',
-        time: '2pm - 3pm',
-        eventName: 'Slushie Apocalypse I',
-        description:
-          'This is totally a description of this really awesome event that will be taking place during this festival at the TangleStage. Be sure to not miss the free slushies cause they are rad!',
-        locationName: 'TangleStage',
-      },
-      {
-        id: 2,
-        day: 'friday',
-        time: '6pm - 7pm',
-        eventName: 'Slushie Apocalypse II',
-        description:
-          'This is totally a description of this really awesome event that will be taking place during this festival at the Yella Yurt. Be sure to not miss the free slushies cause they are rad!',
-        locationName: 'Yella Yurt',
-      },
-    ]
-    res.json({ day, events })
+
+    const display = await db.getEvById(day)
+    res.json({ day, display })
   } catch (e) {
     next(e)
   }
